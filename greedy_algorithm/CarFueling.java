5e2b553d912adb583f251c79a33d1646bbed6c02
HYun66 <niuhy0728@gmail.com>
2020-07-12 14:15:00 +1000

    Car fuelling problem using greedy algorithm

--- greedy_algorithm/CarFueling.java
+++ greedy_algorithm/CarFueling.java
@@ -5,21 +5,14 @@ import java.io.*;
 public class CarFueling {
     static int computeMinRefills(int dist, int tank, int[] stops) {
         int numRefills = 0, currentRefills = 0, lastRefill = 0;
-        int n = stops.length;
-        int[] allStops = Arrays.copyOf(stops, n + 1);
-        allStops[n] = dist;
-        
+        int n = stops.length - 1;
+
         if (dist <= tank) return 0;
  
         while (currentRefills < n) {
             lastRefill = currentRefills;
-            if (currentRefills == 0) {
-                if (allStops[0] <= tank) {
-                    currentRefills += 1;
-                }
-            }
-            
-            while (currentRefills < n && (allStops[currentRefills+1] - allStops[lastRefill] <= tank)) {
+     
+            while (currentRefills < n && (stops[currentRefills + 1] - stops[lastRefill] <= tank)) {
                 currentRefills += 1;
             } 
 
@@ -28,11 +21,7 @@ public class CarFueling {
                 numRefills += 1;
             }
         }
-        if (currentRefills == n) {
-            if (allStops[currentRefills] - allStops[lastRefill] <= tank) {
-                numRefills += 1;
-            }
-        }
+   
         return numRefills;
     }
 
@@ -41,10 +30,12 @@ public class CarFueling {
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
-        int stops[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        int stops[] = new int[n + 2];
+        stops[0] = 0;
+        for (int i = 1; i < n + 1; i++) {
             stops[i] = scanner.nextInt();
         }
+        stops[n + 1] = dist;
 
         System.out.println(computeMinRefills(dist, tank, stops));
     }
